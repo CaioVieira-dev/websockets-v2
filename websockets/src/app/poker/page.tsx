46af65b7dc1c8ef0c77e8daf25f742d8aa7c8d94
Caio Vieira <caiocesarav0208@gmail.com>
2024-09-08 16:57:21 +0000

    feat: :construction: começa o campo

--- websockets/src/app/poker/page.tsx
+++ websockets/src/app/poker/page.tsx
@@ -12,6 +12,7 @@ export default function Poker() {
           <Card />
         </section>
         <Options />
+        <Board />
       </div>
     </div>
   );
@@ -34,6 +35,21 @@ function Options() {
       <button className={buttonClasses}>Configurar cartas</button>
       <button className={buttonClasses}>Limpar cartas</button>
       <button className={buttonClasses}>Virar cartas</button>
+    </section>
+  );
+}
+
+function Board() {
+  return (
+    <section className="flex w-full flex-col gap-2 rounded-3xl bg-gradient-to-r from-green-200 via-green-100 to-green-200 px-4 py-2">
+      <div className="flex items-center justify-between border-b-2 border-green-300 px-4 py-2">
+        <h5 className="text-4xl">Jogador</h5>
+        <h5 className="text-4xl">Pontuação:</h5>
+      </div>
+      <div className="flex items-center justify-between px-4 py-2">
+        <p className="text-4xl">Caio</p>
+        <Card />
+      </div>
     </section>
   );
 }
