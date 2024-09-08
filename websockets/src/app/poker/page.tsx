0e70d66da9ad0c81fcbb0932dc99ce62e2692209
Caio Vieira <caiocesarav0208@gmail.com>
2024-09-08 16:38:29 +0000

    feat: :construction: ajusta estilos e adiciona mock dos botões de opções

--- websockets/src/app/poker/page.tsx
+++ websockets/src/app/poker/page.tsx
@@ -11,6 +11,7 @@ export default function Poker() {
           <Card />
           <Card />
         </section>
+        <Options />
       </div>
     </div>
   );
@@ -18,8 +19,21 @@ export default function Poker() {
 
 function Card() {
   return (
-    <div className="flex h-52 w-40 items-center justify-center rounded-lg bg-green-400 px-2 transition-colors hover:cursor-pointer hover:bg-green-800">
-      <p className="font-mono text-[6.5rem] leading-[6rem]">42</p>
+    <div className="flex h-52 w-40 items-center justify-center rounded-lg bg-gradient-to-r from-green-400 via-green-300 to-green-400 px-2 transition-colors hover:cursor-pointer hover:from-green-700 hover:via-green-600 hover:to-green-700">
+      <p className="font-mono text-[6.5rem] leading-[6rem] text-white">42</p>
     </div>
+  );
+}
+
+function Options() {
+  const buttonClasses =
+    "rounded bg-green-400 px-4 py-2 text-white transition-colors hover:bg-green-600";
+  return (
+    <section className="flex justify-evenly gap-4 py-4">
+      <button className={buttonClasses}>Remover jogadores</button>
+      <button className={buttonClasses}>Configurar cartas</button>
+      <button className={buttonClasses}>Limpar cartas</button>
+      <button className={buttonClasses}>Virar cartas</button>
+    </section>
   );
 }
