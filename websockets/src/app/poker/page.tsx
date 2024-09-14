dd11c4d0e6035940f1fa79076f214c7f8f3d4d59
Caio Vieira <caiocesarav0208@gmail.com>
2024-09-14 00:37:21 +0000

    feat: :construction: adiciona tela para entrar na sala do poker

---
+++ websockets/src/app/poker/page.tsx
@@ -0,0 +1,55 @@
+"use client";
+
+import { type ChangeEvent, useCallback, useState } from "react";
+
+export default function EnterPokerRoom() {
+  const [formState, setFormState] = useState<Record<string, string>>({});
+  const updateForm = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+
+    setFormState((prev) => {
+      console.log({ ...prev });
+      return { ...prev, [e.target.name]: e.target.value };
+    });
+  }, []);
+
+  return (
+    <div className="min-w-screen min-h-screen bg-gradient-to-r from-indigo-500 via-cyan-300 to-indigo-500">
+      <div className="container mx-auto flex min-h-screen flex-col px-4 py-2">
+        <form
+          action=""
+          className="container flex flex-col gap-4 rounded-3xl bg-gradient-to-r from-green-200 via-green-100 to-green-200 p-4 text-2xl"
+        >
+          <h1 className="self-center py-2 text-6xl">{"Poker :)"}</h1>
+          <div className="flex flex-col gap-2">
+            <label htmlFor="name" className="w-full">
+              Nome:
+            </label>
+            <input
+              type="text"
+              name="name"
+              placeholder="Seu nome..."
+              onChange={updateForm}
+              value={formState?.name}
+              className="rounded px-2 py-1"
+            />
+          </div>
+          <div className="flex flex-col gap-2">
+            <label htmlFor="room">Sala:</label>
+            <input
+              type="text"
+              name="room"
+              placeholder="Entre numa sala em andamento..."
+              onChange={updateForm}
+              value={formState?.room}
+              className="rounded px-2 py-1"
+            />
+          </div>
+          <button className="w-full self-center rounded bg-green-300 px-4 py-2 md:max-w-[50%]">
+            {formState?.room ? "Entrar na sala!" : "Criar uma sala"}
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
